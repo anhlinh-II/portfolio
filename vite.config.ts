e2a65f12f698afa41a329db521401ef147b18861
Anh Linh <linhnguyenanh75@gmail.com>
2024-09-07 14:25:21 +0700

    test-3

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     port: 3000
   },
-  base: "/my-portfolio",
+  base: "/my-portfolio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src/"),
